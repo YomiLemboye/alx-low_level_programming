36b73b7b7a1ec0d76fc5eee28c36b312697eb178
yomilemboye <lemboyeyomi@gmail.com>
2023-10-25 14:45:50 -0700

    Main c fixred

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -54,4 +54,22 @@ int op_div(int a, int b)
 	}
 
 	return (a / b);
+}
 
+/**
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
